65eb9ffef5df22629fe2e0812c7b993d8a49bbcc
MilosCica <milos.cica1986@gmail.com>
2023-06-18 17:04:24 +0200

    adds POM pattern

--- src/test/java/org/example/ZadaciTest.java
+++ src/test/java/org/example/ZadaciTest.java
@@ -11,6 +11,24 @@ class ZadaciTest {
     public static WebDriver driver;
     public static ChromeOptions options;
 
+    private static final By userNameField = By.xpath("//input[@id='user-name']");
+    private static final By passwordField = By.cssSelector("#password");
+    private static final By loginButton = By.xpath("//input[@id='login-button']");
+
+    private static final By firstNameField = By.xpath("//input[@id='first-name']");
+    private static final By lastNameField = By.cssSelector("#last-name");
+    private static final By postalCodeField = By.cssSelector("#postal-code");
+    private static final By continueButton = By.xpath("//input[@id='continue']");
+    private static final By checkoutButton = By.xpath("//button[@id='checkout']");
+    private static final By finishButton = By.xpath("//button[@id='finish']");
+    private static final By dodajUKorpuButton = By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']");
+    private static final By otvoriKorpuButton = By.cssSelector(".shopping_cart_link");
+    private static final By isprazniKorpuButton = By.xpath("//button[@id='remove-sauce-labs-backpack']");
+
+    private static final By twitterSignUpButton = By.xpath("//span[@class='css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0'][normalize-space()='Sign up']");
+    private static final By facebookLiteButton = By.cssSelector("a[title='Facebook Lite for Android.']");
+    private static final By instagramJobsButton = By.xpath("//span[normalize-space()='Jobs']");
+
     @BeforeAll
     static void init() {
         options = new ChromeOptions();
@@ -24,44 +42,52 @@ class ZadaciTest {
 
     @Test
     void zadatak1() {
+        goToSite("https://www.saucedemo.com/");
         login("standard_user", "secret_sauce");
     }
 
     @Test
     void zadatak2() {
+        goToSite("https://www.saucedemo.com/");
         login("locked_out_user", "secret_sauce");
         text();
     }
 
     @Test
     void zadatak3() {
+        goToSite("https://www.saucedemo.com/");
         login("problem_user", "secret_sauce");
     }
 
     @Test
     void zadatak4() {
+        goToSite("https://www.saucedemo.com/");
         login("performance_glitch_user", "secret_sauce");
     }
 
     @Test
     void zadatak5() {
+        goToSite("https://www.saucedemo.com/");
         login("standard_user", "code_comtrade");
         text();
     }
     @Test
     void zadatakKupovina() {
+        goToSite("https://www.saucedemo.com/");
         login("standard_user", "secret_sauce");
         buy();
     }
 
     @Test
     void zadatakKorpa() {
+        goToSite("https://www.saucedemo.com/");
         login("standard_user", "secret_sauce");
         korpa();
     }
 
     @Test
     void zadatakPraznaKorpa() {
+        goToSite("https://www.saucedemo.com/");
         login("standard_user", "secret_sauce");
         korpa();
         praznaKorpa();
@@ -69,59 +95,62 @@ class ZadaciTest {
 
     @Test
     void zadatakTwitter() {
-        twitter();
+        goToSite("https://twitter.com/");
+        twitterSignUp();
     }
 
     @Test
     void zadatakFacebook() {
-        facebook();
+        goToSite("https://www.facebook.com/");
+        facebookLite();
     }
 
     @Test
     void zadatakInstagram() {
-        instagram();
+        goToSite("https://www.instagram.com/");
+        instagramJobs();
+    }
+
+    private static void goToSite(String url) {
+        driver.get(url);
     }
 
     private static void login(String username, String password) {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys(username);
-        driver.findElement(By.cssSelector("#password")).sendKeys(password);
-        driver.findElement(By.xpath("//input[@id='login-button']")).click();
+        driver.findElement(userNameField).sendKeys(username);
+        driver.findElement(passwordField).sendKeys(password);
+        driver.findElement(loginButton).click();
     }
 
     private static void buy() {
-        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
-        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
-        driver.findElement(By.xpath("//button[@id='checkout']")).click();
-        driver.findElement(By.xpath("//input[@id='first-name']")).sendKeys("Milos");
-        driver.findElement(By.cssSelector("#last-name")).sendKeys("Cica");
-        driver.findElement(By.cssSelector("#postal-code")).sendKeys("22330");
-        driver.findElement(By.xpath("//input[@id='continue']")).click();
-        driver.findElement(By.xpath("//button[@id='finish']")).click();
+        driver.findElement(dodajUKorpuButton).click();
+        driver.findElement(otvoriKorpuButton).click();
+        driver.findElement(checkoutButton).click();
+        driver.findElement(firstNameField).sendKeys("Milos");
+        driver.findElement(lastNameField).sendKeys("Cica");
+        driver.findElement(postalCodeField).sendKeys("22330");
+        driver.findElement(continueButton).click();
+        driver.findElement(finishButton).click();
     }
 
     private static void korpa() {
-        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
-        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+        driver.findElement(dodajUKorpuButton).click();
+        driver.findElement(otvoriKorpuButton).click();
     }
 
     private static void praznaKorpa() {
-        driver.findElement(By.xpath("//button[@id='remove-sauce-labs-backpack']")).click();
+        driver.findElement(isprazniKorpuButton).click();
     }
 
-    private static void twitter() {
-        driver.get("https://twitter.com/");
-        driver.findElement(By.xpath("//span[@class='css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0'][normalize-space()='Sign up']")).click();
+    private static void twitterSignUp() {
+        driver.findElement(twitterSignUpButton).click();
     }
 
-    private static void facebook() {
-        driver.get("https://www.facebook.com/");
-        driver.findElement(By.cssSelector("a[title='Facebook Lite for Android.']")).click();
+    private static void facebookLite() {
+        driver.findElement(facebookLiteButton).click();
     }
 
-    private static void instagram() {
-        driver.get("https://www.instagram.com/");
-        driver.findElement(By.cssSelector("body > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > section:nth-child(1) > footer:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > a:nth-child(1) > span:nth-child(1)")).click();
+    private static void instagramJobs() {
+        driver.findElement(instagramJobsButton).click();
     }
 
     private static void text() {
